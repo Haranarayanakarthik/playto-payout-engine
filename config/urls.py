76408397992e71bce4142a8389b1a1c8d6a01335
Kommaraju Hara Narayana Karthik <karthikkommaraju1563@gmail.com>
2026-04-27 16:37:05 +0530

    Delete config directory

--- config/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from payouts.views import dashboard, create_payout
-
-urlpatterns = [
-    path("api/v1/dashboard", dashboard),
-    path("api/v1/payouts", create_payout),
-]
